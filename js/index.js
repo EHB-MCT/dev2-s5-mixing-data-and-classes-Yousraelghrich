92887200d943d489a232b91eddd2e2364e0a97c3
yousra <yousra.el.ghrich@student.ehb.be>
2023-03-22 11:22:12 +0100

    oefening gestart

--- js/index.js
+++ js/index.js
@@ -1,15 +1,21 @@
 "use strict";
 
 const app = {
-    items: [],
-    init() {
-    },
-    getData() {
-    },
-    onSearch() {
-    },
-    render(message) {
-    }
+	items: [],
+	init() {},
+	getData() {},
+	onSearch() {},
+	render(message) {},
+};
+app.init();
 
+class Joke {
+	constructor(category, joke, setup, delivery) {
+		this.category = category;
+		this.joke = joke;
+		this.setup = setup;
+		this.delivery = delivery;
+	}
 }
-app.init()
+
+const jokeRacist = new Joke("racist");
